ef62494fdeb1f2c04c5cf50b383edb0dd62db96f
nipil <nipil@users.noreply.github.com>
2024-04-28 21:03:56 +0200

    Migrate addresses detection to regex
    + remove sample files

--- src/iphotmap/iphotmap.py
+++ src/iphotmap/iphotmap.py
@@ -11,7 +11,7 @@ import subprocess
 import sys
 import threading
 import time
-from typing import Optional, Self
+from typing import Optional
 
 ENV_NAME_PATH = 'PATH'
 
@@ -50,108 +50,6 @@ def find_cmd_from_env_path(name: str) -> pathlib.Path:
             continue
         return path
     raise FileNotFoundError(f'{name} not found in ${ENV_NAME_PATH}')
-
-
-class TcpdumpRecord:
-    TOKEN_IN = 'In'
-    TOKEN_OUT = 'Out'
-    TOKEN_MULTI = 'M'
-    TOKEN_IP = 'IP'
-    TOKEN_ARP = 'ARP,'
-    TOKEN_IP6 = 'IP6'
-    TOKEN_IP_UDP = 'UDP'
-    TOKEN_IP_TCP = 'tcp'
-    TOKEN_IP_ICMP = 'ICMP'
-
-    TOKEN_SUPPORTED = {TOKEN_IP: (TOKEN_IP_UDP, TOKEN_IP_TCP, TOKEN_IP_ICMP), }
-
-    MIN_TOKEN_DETECTION = 2
-    ANY_INTERFACE_MARKERS = (TOKEN_IN, TOKEN_OUT, TOKEN_MULTI)
-    ANY_INTERFACE_MARKERS_POSITION = 1
-    ANY_INTERFACE_MARKERS_OFFSET = 2
-    STD_INTERFACE_MARKERS = (TOKEN_IP, TOKEN_IP6, TOKEN_ARP)
-    STD_INTERFACE_MARKERS_POSITION = 0
-    STD_INTERFACE_MARKERS_OFFSET = 0
-
-    OFFSET_PROTO = 0
-    OFFSET_IP_PROTO_SUB = 4
-    OFFSET_IP_ADDRESSES = (1, 3)
-
-    SPLIT_RE = re.compile(r'\s+')
-    STARTING_IP4_RE = re.compile(r'^(\d+\.\d+\.\d+\.\d+)')
-
-    @classmethod
-    def is_protocol_supported(cls, proto: str) -> bool:
-        return proto in cls.TOKEN_SUPPORTED
-
-    @classmethod
-    def is_sub_protocol_supported(cls, proto: str, sub_proto: str) -> bool:
-        if not cls.is_protocol_supported(proto):
-            raise ValueError(f'Unknown protocol {proto}, cannot check sub-protocol {sub_proto}')
-        return sub_proto in cls.TOKEN_SUPPORTED[proto]
-
-    @classmethod
-    def _detect_format_offset(cls, tokens: list[str]) -> Optional[int]:
-        if len(tokens) >= 2 and tokens[cls.ANY_INTERFACE_MARKERS_POSITION] in cls.ANY_INTERFACE_MARKERS:
-            return cls.ANY_INTERFACE_MARKERS_OFFSET
-        if len(tokens) >= 1 and tokens[cls.STD_INTERFACE_MARKERS_POSITION] in cls.STD_INTERFACE_MARKERS:
-            return cls.STD_INTERFACE_MARKERS_OFFSET
-        return None
-
-    @classmethod
-    def detect_format_offset_from_line(cls, line) -> Optional[int]:
-        tokens = cls._tokenize_line(line)
-        return cls._detect_format_offset(tokens)
-
-    @classmethod
-    def _tokenize_line(cls, line: str) -> list[str]:
-        return cls.SPLIT_RE.split(line.strip())
-
-    @classmethod
-    def _get_protocol(cls, tokens: list[str], starting_offset: int) -> str:
-        try:
-            return tokens[cls.OFFSET_PROTO + starting_offset]
-        except IndexError:
-            raise ValueError(f'Not enough tokens to detect protocol: {tokens}')
-
-    @classmethod
-    def _get_ip_sub_protocol(cls, tokens: list[str], starting_offset: int) -> str:
-        # same indices for TOKEN_IP_UDP, TOKEN_IP_TCP, TOKEN_IP_ICMP
-        try:
-            return tokens[cls.OFFSET_IP_PROTO_SUB + starting_offset]
-        except IndexError:
-            raise ValueError(f'Not enough tokens to detect sub-protocol: {tokens}')
-
-    @classmethod
-    def _get_ip_addresses(cls, tokens: list[str], starting_offset: int) -> Optional[tuple[str, ...]]:
-        # Same indices and extraction process for TOKEN_IP_UDP, TOKEN_IP_TCP, TOKEN_IP_ICMP
-        try:
-            matches = tuple(cls.STARTING_IP4_RE.match(tokens[i + starting_offset]) for i in cls.OFFSET_IP_ADDRESSES)
-        except IndexError:
-            raise ValueError(f'Not enough tokens to get ip addresses: {tokens}')
-        addresses = tuple(match[0] for match in matches if match is not None)
-        if len(matches) != len(addresses):
-            raise ValueError(f'Some addresses could not be extracted: {tokens}')
-        return addresses
-
-    @classmethod
-    def _addresses_from_ip_line(cls, tokens: list[str], starting_offset: int) -> Optional[Self]:
-        proto_sub = cls._get_ip_sub_protocol(tokens, starting_offset)
-        if not cls.is_sub_protocol_supported(cls.TOKEN_IP, proto_sub):
-            return None
-        return cls._get_ip_addresses(tokens, starting_offset)
-
-    @classmethod
-    def addresses_from_line(cls, line: str, starting_offset: int) -> Optional[tuple[str, ...]]:
-        if starting_offset is None or starting_offset < 0:
-            raise ValueError(f'Invalid starting offset {starting_offset}')
-        tokens = cls._tokenize_line(line)
-        proto = cls._get_protocol(tokens, starting_offset)
-        if not cls.is_protocol_supported(proto):
-            return None
-        if proto == cls.TOKEN_IP:
-            return cls._addresses_from_ip_line(tokens, starting_offset)
-        return None
 
 
 class TcpdumpThread(threading.Thread):
@@ -240,25 +138,15 @@ class TcpdumpThread(threading.Thread):
         last_time = get_time_int()
         # TODO: do not block on line read if no packet captured, so summary and notification still happen "on time"
         for line in stream:
+            # extract
             addresses = self.addresses_from_line(line)
             if addresses is None:
                 print(line.strip())
-
-            # detect
-            # if self._detected_offset is None:
-            #     self._detected_offset = TcpdumpRecord.detect_format_offset_from_line(line)
-            #     if self._detected_offset is None:
-            #         logging.info(f'Skipping packet until format detection succeeds : {line}')
-            #         continue
-            #     logging.info(f'Successfully detected format offset: {self._detected_offset}')
-            # extract
-            # addresses = TcpdumpRecord.addresses_from_line(line, self._detected_offset)
-            # if addresses is None:
-            #     continue
+                continue
             # update
             current_time = get_time_int()
-            # for address in addresses:
-            #     seen_address[address] = current_time
+            for address in addresses:
+                seen_address[address] = current_time
             # shutdown handler
             if self._termination_flag.is_set():
                 break
